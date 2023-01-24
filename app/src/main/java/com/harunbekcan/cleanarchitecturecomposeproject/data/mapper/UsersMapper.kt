51011b73e185b74c0ed99bfc8d80606feb9b6cd6
Harun Bekcan <harunbekcan@gmail.com>
2023-01-24 13:45:24 +0300

    UsersMapper

--- app/src/main/java/com/harunbekcan/cleanarchitecturecomposeproject/data/mapper/UsersMapper.kt
+++ app/src/main/java/com/harunbekcan/cleanarchitecturecomposeproject/data/mapper/UsersMapper.kt
@@ -1,5 +1,18 @@
 package com.harunbekcan.cleanarchitecturecomposeproject.data.mapper
 
+import com.harunbekcan.cleanarchitecturecomposeproject.data.response.UserResponseItem
+import com.harunbekcan.cleanarchitecturecomposeproject.domain.uimodel.UserUiModel
+
 open class UsersMapper {
 
+    fun UserResponseItem.toUserUiModel() = UserUiModel(
+        address = address,
+        company = company,
+        email = email.orEmpty(),
+        id = id ?: -1,
+        name = name.orEmpty(),
+        phone = phone.orEmpty(),
+        username = username.orEmpty(),
+        website = website.orEmpty()
+    )
 }
--- app/src/main/java/com/harunbekcan/cleanarchitecturecomposeproject/domain/uimodel/UserUiModel.kt
+++ app/src/main/java/com/harunbekcan/cleanarchitecturecomposeproject/domain/uimodel/UserUiModel.kt
@@ -4,8 +4,8 @@ import com.harunbekcan.cleanarchitecturecomposeproject.data.response.Address
 import com.harunbekcan.cleanarchitecturecomposeproject.data.response.Company
 
 data class UserUiModel(
-    val address: Address,
-    val company: Company,
+    val address: Address?,
+    val company: Company?,
     val email: String,
     val id: Int,
     val name: String,
