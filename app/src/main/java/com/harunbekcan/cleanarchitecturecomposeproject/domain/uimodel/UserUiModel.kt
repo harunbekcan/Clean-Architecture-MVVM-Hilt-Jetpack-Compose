522334fdccc7237879f5c42d43ed53a2af19dd87
Harun Bekcan <harunbekcan@gmail.com>
2023-01-24 13:34:24 +0300

    UserUiModel created.

---
+++ app/src/main/java/com/harunbekcan/cleanarchitecturecomposeproject/domain/uimodel/UserUiModel.kt
@@ -0,0 +1,15 @@
+package com.harunbekcan.cleanarchitecturecomposeproject.domain.uimodel
+
+import com.harunbekcan.cleanarchitecturecomposeproject.data.response.Address
+import com.harunbekcan.cleanarchitecturecomposeproject.data.response.Company
+
+data class UserUiModel(
+    val address: Address,
+    val company: Company,
+    val email: String,
+    val id: Int,
+    val name: String,
+    val phone: String,
+    val username: String,
+    val website: String,
+)
