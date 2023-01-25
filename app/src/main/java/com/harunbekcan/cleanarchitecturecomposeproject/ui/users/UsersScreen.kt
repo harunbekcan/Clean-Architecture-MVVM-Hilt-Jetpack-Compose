4872e279097666b4152a1d9079a8ad774ba0b52d
Harun Bekcan <harunbekcan@gmail.com>
2023-01-25 20:21:32 +0300

    UsersViewModel created.

--- app/src/main/java/com/harunbekcan/cleanarchitecturecomposeproject/ui/users/UsersScreen.kt
+++ app/src/main/java/com/harunbekcan/cleanarchitecturecomposeproject/ui/users/UsersScreen.kt
@@ -4,25 +4,26 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Surface
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-
+@Preview
 @Composable
 fun UsersScreen() {
-    Surface(
+    Card(
         modifier = Modifier.fillMaxSize()
     ) {
         Column {
             Text(
-                "Users Page", modifier = Modifier
+                "Users", modifier = Modifier
                     .fillMaxWidth()
                     .align(alignment = Alignment.CenterHorizontally)
                     .padding(top = 20.dp),
