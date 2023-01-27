0e4a81372cb840aff05359004e16243f3511068b
Harun Bekcan <harunbekcan@gmail.com>
2023-01-27 16:30:43 +0300

    Resource fixed.

--- app/src/main/java/com/harunbekcan/cleanarchitecturecomposeproject/utils/Resource.kt
+++ app/src/main/java/com/harunbekcan/cleanarchitecturecomposeproject/utils/Resource.kt
@@ -1,7 +1,7 @@
 package com.harunbekcan.cleanarchitecturecomposeproject.utils
 
-sealed class Resource<T>(val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T) : Resource<T>(data)
-    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
-    class Loading<T> : Resource<T>()
+sealed class Resource<out T : Any> {
+    object Loading : Resource<Nothing>()
+    data class Success<out T : Any>(val data: T) : Resource<T>()
+    data class Error(val errorMessage: String) : Resource<Nothing>()
 }
